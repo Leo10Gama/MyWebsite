ad6c77cc5cc6fac8f86fc5cc268e2823d1ced495
Leonardo Gama <leo10gama@gmail.com>
2021-01-10 15:11:17 -0500

    parallax progress...?

--- Scripts/gama.js
+++ Scripts/gama.js
@@ -1,1 +1,15 @@
-// Nothing to see here (yet!)
+// When the screen scrolls
+window.addEventListener("scroll", () => {
+    // Parallax stuff
+    const translate = document.querySelectorAll(".parallax");
+    let scroll = window.pageYOffset;
+
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        var position = element.getBoundingClientRect();
+        // Only do parallax if the thing is on the screen
+        if ((position.top >= 0 && position.bottom <= window.innerHeight) || (position.top < window.innerHeight && position.bottom >= 0)) {
+            element.style.transform = `translateY(${(scroll - position.top) * speed}px)`;
+        }
+    })
+})

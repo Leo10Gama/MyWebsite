ea0c64f83680863959f59e9e084f8f456060b17c
Leonardo Gama <leo10gama@gmail.com>
2021-01-11 20:50:10 -0500

    Made everything separate pages (to fix the parallax that wasn't working :/ ) and also tweaked some css and styling stuff (like replacing img3.png because it was down-right terrifying me)

--- Scripts/gama.js
+++ Scripts/gama.js
@@ -7,6 +7,7 @@ window.addEventListener("scroll", () => {
     translate.forEach(element => {
         let speed = element.dataset.speed;
         var position = element.getBoundingClientRect();
+
         // Only do parallax if the thing is on the screen
         if ((position.top >= 0 && position.bottom <= window.innerHeight) || (position.top < window.innerHeight && position.bottom >= 0)) {
             element.style.transform = `translateY(${(scroll * speed)}px)`;

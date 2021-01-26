269ce820aafe742e2b4d06569bc0c855c2ff7f48
Leonardo Gama <leo10gama@gmail.com>
2021-01-26 13:40:41 -0500

    Finally implemented the gallery page!

--- Scripts/gama.js
+++ Scripts/gama.js
@@ -1,7 +1,7 @@
 // When the screen scrolls
 window.addEventListener("scroll", () => {
     // Parallax stuff
-    const translate = document.querySelectorAll(".parallax");
+    const translate = document.querySelectorAll(".parallax, .gallery-img");
     let scroll = window.pageYOffset;
 
     translate.forEach(element => {
@@ -9,8 +9,23 @@ window.addEventListener("scroll", () => {
         var position = element.getBoundingClientRect();
 
         // Only do parallax if the thing is on the screen
-        if ((position.top >= 0 && position.bottom <= window.innerHeight) || (position.top < window.innerHeight && position.bottom >= 0)) {
-            element.style.transform = `translateY(${(scroll * speed)}px)`;
-        }
+        element.style.transform = `translateY(${(scroll * speed)}px)`;
     })
 })
+
+/* This function gives each gallery image a random offset, as well as
+ * a random scale value (within a given range, of course), which then
+ * affects the data-speed value given, affecting its parallax. Thus,
+ * larger images will scroll more quickly than smaller images to mimic
+ * actual parallax
+ */
+// function setGalleryParallax(imageCaller) {
+//     // Generate a random offset (from the left or right)
+//     var offset = (Math.random() * 1000) / 5;
+//     // Generate the scale of the image (up to 3x as large)
+//     var scale = Math.floor(Math.random() * 3) + 0.75;
+//     var speed = (scale < 1) ? (- (scale / 2)) : (scale / 2);
+//     var zIndex = scale + 20;
+//     imageCaller.setAttribute("style", `transform: scale(${scale}, ${scale}) translateX(${offset}%); z-index: ${zIndex};`);
+//     // imageCaller.setAttribute("data-speed", speed);
+// }
